d5c7ce0ae050af3f986c089af277292cad411d3f
Kittipong2001 <128574773+Kittipong2001@users.noreply.github.com>
2023-04-10 09:22:30 +0700

    Add functionality

--- script.js
+++ script.js
@@ -8,17 +8,63 @@ function Book(author, title, pages) {
 }
 
 function addBookToLibrary() {
-	// do stuff here
+	table.innerHTML = "";
+	myLibrary.forEach((element, index) => {
+		let htmlString = `<div id="${index}">
+        <h3>${element.title}</h3>
+        <h3>${element.author}</h3>
+        <h3>${element.pages} pages</h3>
+        <button class="${element.isRead ? "read" : "unread"}" id="read-status">
+                ${element.isRead ? "Read" : "Unread"}
+        </button>
+        <button id="remove">Remove</button>
+        </div>`;
+
+		table.insertAdjacentHTML("beforeend", htmlString);
+	});
+
+	document.querySelectorAll("#remove").forEach((button) => {
+		button.addEventListener("click", (e) => {
+			let id = parseInt(e.target.parentElement.id);
+			myLibrary.splice(id, 1);
+			addBookToLibrary();
+		});
+	});
+
+	document.querySelectorAll("#read-status").forEach((button) => {
+		button.addEventListener("click", (e) => {
+			let id = parseInt(e.target.parentElement.id);
+			myLibrary[id].isRead = !myLibrary[id].isRead;
+			addBookToLibrary();
+		});
+	});
 }
 
 const addButton = document.querySelector("#add");
 const submitButton = document.querySelector(".submit");
 const toggleForm = document.querySelector(".pop-up");
+const submitForm = document.getElementById("add-book");
+const table = document.querySelector(".content");
+
+// initialize
 
 addButton.addEventListener("click", () => {
 	toggleForm.classList.remove("hidden");
 });
 
-submitButton.addEventListener("click", () => {
+submitForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+	// will be triggered once submit button is clicked, then push a new object into the myLibrary array
+	let title = document.getElementById("title");
+	let author = document.getElementById("author");
+	let pages = document.getElementById("pages");
+
+	myLibrary.push(new Book(author.value, title.value, pages.value));
+
+	// clear form
+	e.target.reset();
+
+	// remove overlay
 	toggleForm.classList.add("hidden");
+	addBookToLibrary();
 });
